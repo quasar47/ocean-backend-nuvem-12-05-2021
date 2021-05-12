7d63b07c19fff1b0f31cf7041964bfdc3b3e77f6
Antonio Cintra <antonio.cintra@outlook.com>
2021-05-12 12:26:09 -0300

    Endpoints com MongoDB e Configuração MongoDB Atlas

--- index.js
+++ index.js
@@ -1,85 +1,82 @@
 const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 
-(async () => {
+(async() => {
 
-const url = 'mongodb://localhost:27017';
+    //const url = 'mongodb://localhost:27017';
+    const url = 'mongodb+srv://admin:JNolEdsOSA3QMPEk@cluster0.wvzu2.mongodb.net/ocean_db?retryWrites=true&w=majority'
 
-const dbName = 'ocean_bancodados_11_05_2021';
+    //const dbName = 'ocean_bancodedados_11_05_2021';
+    const dbName = 'ocean_db';
 
-console.info('Conectando ao banco de dados...');
+    console.info('Conectando ao banco de dados...');
 
-const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
-console.info('MongoDB conectado com sucesso!!');
+    console.info('MongoDB conectado com sucesso!!');
 
-const db = client.db(dbName);
+    const db = client.db(dbName);
 
-const app = express();
+    const app = express();
 
-app.use(express.json());
+    app.use(express.json());
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World');
-});
+    app.get('/hello', function(req, res) {
+        res.send('Hello World');
+    });
 
-const mensagens = ['Essa é a primeira mensagem!', 'Essa é a segunda mensagem!'];
+    const mensagensCollection = db.collection('mensagens');
 
-const mensagensCollection = db.collection('mensagens');
+    // CRUD (Create, Read, Update, Delete)
 
-// CRUD (Create, Read, Update, Delete)
+    // GET: READ ALL (exibir todos os registros)
+    app.get('/mensagens', async(req, res) => {
+        const listaMensagens = await mensagensCollection.find().toArray();
 
-// GET: READ ALL (exibir todos os registros)
-app.get('/mensagens', async (req, res) => {
-  const listaMensagens = await mensagensCollection.find().toArray();
+        res.send(listaMensagens);
+    });
 
-  res.send(listaMensagens);
-});
+    // GET: READ SINGLE (exibir apenas um registro)
+    app.get('/mensagens/:id', async(req, res) => {
+        const id = req.params.id;
 
-// GET: READ SINGLE (exibir apenas um registro)
-app.get('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+        const mensagem = await mensagensCollection.findOne({ _id: ObjectId(id) });
 
-  const mensagem = mensagens[id];
+        if (!mensagem) {
+            res.send('Mensagem não encontrada.');
+        }
 
-  if (!mensagem) {
-    res.send('Mensagem não encontrada.');
-  }
+        res.send(mensagem);
+    });
 
-  res.send(mensagem);
-});
+    // POST: CREATE (criar um registro)
+    app.post('/mensagens', async(req, res) => {
+        const mensagem = req.body;
 
-// POST: CREATE (criar um registro)
-app.post('/mensagens', (req, res) => {
-  const mensagem = req.body.mensagem;
+        await mensagensCollection.insertOne(mensagem);
 
-  mensagens.push(mensagem);
+        res.send(mensagem);
+    });
 
-  const id = mensagens.length;
+    // PUT: UPDATE (editar um registro)
+    app.put('/mensagens/:id', async(req, res) => {
+        const id = req.params.id;
+        const mensagem = req.body;
 
-  res.send(`Mensagem '${id}' criada com sucesso.`);
-});
+        await mensagensCollection.updateOne({ _id: ObjectId(id) }, { $set: mensagem });
 
-// PUT: UPDATE (editar um registro)
-app.put('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
+        res.send('Mensagem atualizada com sucesso.');
+    });
 
-  const mensagem = req.body.mensagem;
+    // DELETE: DELETE (remover um registro)
+    app.delete('/mensagens/:id', async(req, res) => {
+        const id = req.params.id;
 
-  mensagens[id] = mensagem;
+        await mensagensCollection.deleteOne({ _id: ObjectId(id) });
 
-  res.send('Mensagem atualizada com sucesso.');
-});
+        res.send('Mensagem removida com sucesso.');
+    });
 
-// DELETE: DELETE (remover um registro)
-app.delete('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
-
-  delete mensagens[id];
-
-  res.send('Mensagem removida com sucesso.');
-});
-
-app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 
 })();
